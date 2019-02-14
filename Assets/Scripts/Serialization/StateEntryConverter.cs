3d789932656888b0d3acf20ef8f6742d4c85caa7
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2019-02-14 11:14:15 +0800

    Experiment with serialization

---
+++ Assets/Scripts/Serialization/StateEntryConverter.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+// TODO: Try again using JsonConverter<T> when package is upgraded
+public class StateEntryConverter<T> : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return true;
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        return new StateEntry<T>((T)reader.Value);
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        StateEntry<T> entry = (StateEntry<T>)value;
+        writer.WriteValue(entry.Value);
+    }
+}
