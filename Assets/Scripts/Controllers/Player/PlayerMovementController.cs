f5511ae42d2d3642148eb1961c9debf79b05faa2
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2019-02-14 21:26:52 +0800

    Let player drag player's light around the house

---
+++ Assets/Scripts/Controllers/Player/PlayerMovementController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Converts user input to player movement state changes.
+/// </summary>
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxSpeed = 10f;
+
+    [SerializeField]
+    private float minDistance = 0.001f;
+
+    [SerializeField]
+    private LayerMask inputHitLayerMask = 0;
+
+    private Vector3 prevPointerPosition;
+    private Vector3 prevInputPosition;
+
+    private void Start()
+    {
+        prevInputPosition = GetPlayerPosition();
+    }
+    
+    private void Update()
+    {
+        Vector3 nextPosition = Vector3.Lerp(GetPlayerPosition(), GetInputPosition(), Time.smoothDeltaTime * maxSpeed);
+
+        if (Vector3.Distance(GetPlayerPosition(), nextPosition) >= minDistance)
+        {
+            SetPlayerPosition(nextPosition);
+        }
+    }
+    
+    private Vector3 GetPlayerPosition()
+    {
+        return State.Instance.PlayerPosition.Value;
+    }
+
+    private void SetPlayerPosition(Vector3 value)
+    {
+        State.Instance.PlayerPosition.Value = value;
+    }
+
+    private Vector3 GetInputPosition()
+    {   
+        if (Input.GetMouseButton(0) && prevPointerPosition != Input.mousePosition)
+        {
+            prevPointerPosition = Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(prevPointerPosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 10f, inputHitLayerMask))
+            {
+                prevInputPosition = hit.point;
+            }
+        }
+        
+        return prevInputPosition;
+    }
+}
--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -2,5 +2,5 @@
 
 public class State : BehaviourSingleton<State>
 {
-    public StateEntry<Vector2> PlayerPosition = new StateEntry<Vector2>(Vector2.up);
+    public StateEntry<Vector3> PlayerPosition = new StateEntry<Vector3>();
 }
